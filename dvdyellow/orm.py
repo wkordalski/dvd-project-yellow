ff71bf2923cb03bdae194d086a6bf8c1533eaab3
Przemysław Sadownik <ps360958@students.mimuw.edu.pl>
2016-05-16 18:02:38 +0200

    Added ranking changes to orm.py

--- dvdyellow/orm.py
+++ dvdyellow/orm.py
@@ -3,7 +3,7 @@ Here will be classes stored in database.
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 
 Database = declarative_base()
 
@@ -17,6 +17,20 @@ class User(Database):
     id = Column(Integer, primary_key=True)
     name = Column(String(64), unique=True)
     password = Column(String)
+    ranking = Column(Float)
+
+
+class GameResult(Database):
+    """
+    Stores data about finished games
+    """
+    __tablename__ = "gameresults"
+    id = Column(Integer, primary_key=True)
+    player1 = Column(Integer, ForeignKey(User.id))
+    points1 = Column (Float)
+    player2 = Column(Integer, ForeignKey(User.id))
+    points2 = Column (Float)
+    winner = Column(Integer)
 
 
 class GameBoard(Database):
